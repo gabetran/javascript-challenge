c419ec1343b049a4dc4bbb28d41d8786c80371f9
Gabe Tran <gabetran@uw.edu>
2014-10-30 10:22:17 -0700

    added event listener, started the onReady function

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,16 @@
-/*
-    Signup Form Script
-    This script will load the state select list and validate the form before submission
-*/
+
+function onReady() {
+	var elem = document.getElementById("state");
+		for(var i =0; i < usStates.length; i++) {
+			var opt = document.createElement("option");
+			opt.value = usStates[i];
+			var text = document.createTextNode(usStates[i]);
+			opt.appendChild(text);
+			elem.appendChild(opt);
+		}
+
+		console.log("onready is being called");
+}
+
+
+document.addEventListener('DOMContentLoaded', onReady);
